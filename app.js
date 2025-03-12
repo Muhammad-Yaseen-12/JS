0220f784d068786138d9fd0e272aebd4bac1547a
Muhammad Yaseen <myn121myn@gmail.com>
2025-03-12 22:26:37 +0500

    JS

---
+++ app.js
@@ -0,0 +1,47 @@
+// // let str = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias corporis voluptate tenetur quis aut optio totam quod veniam, aspernatur quae officia fuga recusandae blanditiis? Maiores temporibus beatae expedita architecto mollitia!';
+// function btn() {
+
+//     let input = document.getElementById('input');
+//     let str = input.value;
+//     console.log(str);
+
+//     let copy = str.split(" ")
+
+//     console.log(copy);
+//     console.log(copy[0]);
+
+//     for (let i = 0; i < copy.length; i++) {
+//         console.log(i);
+//         copy[i] = copy[i].charAt(0).toUpperCase() + copy[i].slice(1)
+//         console.log(copy[i] = copy[i].charAt(0).toUpperCase() + copy[i].slice(1));
+
+//         console.log(i);
+//         console.log(copy[i]);
+
+
+//     }
+//     let word = copy.join(" ")
+//     // document.writeln(copy)
+//     document.writeln(word)
+// }
+
+
+// function btn() {
+//     let input = document.getElementById("input");
+//     let value = +input.value;
+//     let result = 1
+//     for (let i = value; i > 0; i--) {
+
+//         console.log(i);
+//         result *= i
+
+//     }
+//     console.log(result);
+
+// }
+
+
+    // console.log(Math.floor(1.9));
+    // console.log(Math.round(1.4));
+    // console.log(Math.ceil(1.4));
+    // console.log(Math.floor(10000 + Math.random() * 90000));
